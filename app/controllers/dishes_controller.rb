adb3e8b250880517a6d26f637cd45342dbd32bbe
fukuda0801 <nagisano0801@gmail.com>
2024-02-25 06:40:56 +0900

    dish: activerecord N+1解消

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -13,13 +13,13 @@ class DishesController < ApplicationController
 
   def search
     @q = Dish.ransack(params[:q])
-    @results = @q.result(distinct: true).page(params[:page])
+    @results = @q.result(distinct: true).with_attached_image.page(params[:page])
     @genre_selected = params.dig(:q, :genre_eq)
   end
 
   def condition
     @selected_tag_names = params.dig(:search, :tags).values.reject(&:blank?)
-    @dishes = Dish.search_by_tag_names(@selected_tag_names).page(params[:page])
+    @dishes = Dish.search_by_tag_names(@selected_tag_names).with_attached_image.page(params[:page])
   end
 
   private
