cf9a6868320006e1a68b645ffb7a2eddc600690c
fukuda0801 <nagisano0801@gmail.com>
2024-02-25 05:19:12 +0900

    dish: dish関連ページレスポンシブ対応

---
+++ spec/models/dish_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Dish, type: :model do
+  describe "Userモデルのバリデーション" do
+  end
+
+  describe "self.random" do
+  end
+
+  describe "self.search_by_tag_names(tag_names)" do
+  end
+end
