277ad8250f9daf454c7fa8385b0031abd738b3b1
fukuda0801 <nagisano0801@gmail.com>
2024-03-03 09:55:44 +0900

    db: seedファイル内容追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 AdminUser.create!(email: 'admin@example.com', password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD']) if Rails.env.production?
+
+User.create!(
+  name: "管理ユーザー",
+  email: "user@example.com",
+  password: "password",
+  password_confirmation: "password",
+  confirmed_at: Time.now
+  sex: "男"
+)
+
+tag_egg = Tag.create!(
+  name: "卵",
+  category: "食材"
+)
+
+dish = Dish.create!(
+  name: "オムライス",
+  description: "卵をふんだんに使ったオムライス。",
+  cook_time: 20,
+  calorie: 500,
+  difficulty: "初心者",
+  genre: "洋食",
+  category_full_id: "14-121"
+)
+
+dish.tags << tag_egg
