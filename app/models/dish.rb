b35ea4693e787e37eb776392ea59aa60b4bdb601
fukuda0801 <nagisano0801@gmail.com>
2024-03-12 01:46:16 +0900

    dish:　料理詳細ページにてお気に入り登録者数を確認できるよう変更

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -22,6 +22,10 @@ class Dish < ApplicationRecord
     Dish.joins(:tags).where(tags: { name: tag_names }).distinct
   end
 
+  def favorites_count
+    users.count
+  end
+
   def self.ransackable_associations(_auth_object = nil)
     ['tags']
   end
