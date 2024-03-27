ff84d7ce0913ed8e26b3cf760f2988bd8a830821
fukuda0801 <nagisano0801@gmail.com>
2024-03-27 14:17:15 +0900

    comment: activeadminによる管理ページを作成

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :dish
 
   validates :content, presence: true, length: { maximum: 50 }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[content user_id dish_id]
+  end
 end
